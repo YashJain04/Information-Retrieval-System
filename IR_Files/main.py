d41a8fc92a4d54829aafd9c4f31c32f9c692f1c4
Shack <spark188@uottawa.ca>
2025-02-08 21:33:52 -0500

    Small Comment Fix

--- IR_Files/main.py
+++ IR_Files/main.py
@@ -31,8 +31,8 @@ start_time = time.time() # start the timer
 print("Preprocessing documents")
 documents = parse_documents_from_file(doc_folder_path)
 
-#documents = preprocess_documents(documents)
-documents = preprocess_documents_head_only(documents)
+documents = preprocess_documents(documents)
+#documents = preprocess_documents_head_only(documents)
 # if you want to preprocess only the head of the documents, use the above line instead, and uncomment the line above it
 
 documents = preprocess_documents(parse_documents_from_file(doc_folder_path))
@@ -48,8 +48,8 @@ print("")
 start_time = time.time() # start the timer
 print("Building an inverted index.")
 
-#inverted_index = build_inverted_index(documents)
-inverted_index = build_inverted_index_head_only(documents)
+inverted_index = build_inverted_index(documents)
+#inverted_index = build_inverted_index_head_only(documents)
 # if you want to build the inverted index using only the head of the documents, use the above line instead, and uncomment the line above it
 
 save_inverted_index(inverted_index, index_file_path)
@@ -61,8 +61,8 @@ print(f"Time taken to complete STEP 2 (BUILD INVERTED INDEX): {end_time - start_
 print("")
 start_time = time.time() # start the timer
 
-#doc_lengths = calculate_document_lengths(documents)
-doc_lengths = calculate_document_lengths_head_only(documents)
+doc_lengths = calculate_document_lengths(documents)
+#doc_lengths = calculate_document_lengths_head_only(documents)
 # if you want to calculate the document lengths using only the head of the documents, use the above line instead, and uncomment the line above it
 
 results_file = "Results.txt"   # change to Results.json for other version formatting
